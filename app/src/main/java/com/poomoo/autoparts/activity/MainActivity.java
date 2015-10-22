74ca3571177d627cafdc075801a81eaa9923599d
poomoo <454592359@qq.com>
2015-10-22 15:02:55 +0800

    second

---
+++ app/src/main/java/com/poomoo/autoparts/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.poomoo.autoparts.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.poomoo.autoparts.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
--- app/src/main/java/com/poomoo/autoparts/activity/SplashActivity.java
+++ app/src/main/java/com/poomoo/autoparts/activity/SplashActivity.java
@@ -1,14 +1,16 @@
-package com.poomoo.autoparts;
+package com.poomoo.autoparts.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import com.poomoo.autoparts.R;
+
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 
 
